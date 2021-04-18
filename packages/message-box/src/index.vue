aca495cbada6a927ee7599a51bf27eca070ad487
inottn <inottn@outlook.com>
2021-04-18 23:56:27 +0800

    fix(message-box): fix the message box opens repeatedly on press enter

--- packages/message-box/src/index.vue
+++ packages/message-box/src/index.vue
@@ -37,9 +37,6 @@
             @click="
               handleAction(distinguishCancelAndClose ? 'close' : 'cancel')
             "
-            @keydown.enter="
-              handleAction(distinguishCancelAndClose ? 'close' : 'cancel')
-            "
           >
             <i class="el-message-box__close el-icon-close"></i>
           </button>
@@ -84,7 +81,6 @@
             :round="roundButton"
             :size="buttonSize || 'small'"
             @click="handleAction('cancel')"
-            @keydown.enter="handleAction('cancel')"
           >
             {{ cancelButtonText || t('el.messagebox.cancel') }}
           </el-button>
@@ -97,7 +93,6 @@
             :disabled="confirmButtonDisabled"
             :size="buttonSize || 'small'"
             @click="handleAction('confirm')"
-            @keydown.enter="handleAction('confirm')"
           >
             {{ confirmButtonText || t('el.messagebox.confirm') }}
           </el-button>
